05ded35559e375d99ce109d383c94c9f195b4883
jacky <jackylvm@foxmail.com>
2023-07-04 16:19:01 +0800

    有限状态机和基础状态

---
+++ Assets/Scripts/Common/FSM/EntityState.cs
@@ -0,0 +1,22 @@
+﻿// *************************************************************************************
+// FileName: EntityState.cs
+// Description:
+// 
+// Version: v1.0.0
+// Creator: Jacky(jackylvm@foxmail.com)
+// CreationTime: 2023-07-04 15:07:35
+// ==============================================================
+// History update record:
+// 
+// ==============================================================
+// *************************************************************************************
+
+namespace Common.FSM
+{
+    public abstract class EntityState<TEntityType>
+    {
+        public abstract void Enter(TEntityType entity);
+        public abstract void Execute(TEntityType entity, float deltaTime);
+        public abstract void Exit(TEntityType entity);
+    }
+}
---
+++ Assets/Scripts/Common/FSM/StateMachine.cs
@@ -0,0 +1,63 @@
+﻿// *************************************************************************************
+// FileName: StateMachine.cs
+// Description:
+// 
+// Version: v1.0.0
+// Creator: Jacky(jackylvm@foxmail.com)
+// CreationTime: 2023-07-04 15:09:00
+// ==============================================================
+// History update record:
+// 
+// ==============================================================
+// *************************************************************************************
+
+namespace Common.FSM
+{
+    public class StateMachine<TEntityType>
+    {
+        private readonly TEntityType m_Owner;
+
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        public EntityState<TEntityType> CState { get; set; }
+
+        /// <summary>
+        /// 上一状态
+        /// </summary>
+        public EntityState<TEntityType> PState { get; set; }
+
+        /// <summary>
+        /// 全局状态
+        /// </summary>
+        public EntityState<TEntityType> GState { get; set; }
+
+        public StateMachine(TEntityType owner)
+        {
+            m_Owner = owner;
+
+            CState = null;
+            PState = null;
+            GState = null;
+        }
+
+        public void Update(float deltaTime)
+        {
+            GState?.Execute(m_Owner, deltaTime);
+            CState?.Execute(m_Owner, deltaTime);
+        }
+
+        public void ChangeState(EntityState<TEntityType> state)
+        {
+            PState = CState;
+            CState?.Exit(m_Owner);
+            CState = state;
+            CState?.Enter(m_Owner);
+        }
+
+        public void RevertToPreviousState()
+        {
+            ChangeState(PState);
+        }
+    }
+}
