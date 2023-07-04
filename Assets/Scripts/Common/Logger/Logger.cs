e88c6f21016c439dea382d731dc783b3fb91caaf
jacky <jackylvm@foxmail.com>
2023-07-04 16:18:35 +0800

    Logger

---
+++ Assets/Scripts/Common/Logger/Logger.cs
@@ -0,0 +1,34 @@
+﻿// *************************************************************************************
+// FileName: Logger.cs
+// Description:
+// 
+// Version: v1.0.0
+// Creator: Jacky(jackylvm@foxmail.com)
+// CreationTime: 2023-07-04 15:53:16
+// ==============================================================
+// History update record:
+// 
+// ==============================================================
+// *************************************************************************************
+
+using MS.Log4Unity;
+
+namespace Common.Logger
+{
+    public static class Logger
+    {
+        // private class Nested
+        // {
+        //     internal static readonly Logger ClassInstance = Activator.CreateInstance(typeof(Logger), true) as Logger;
+        //
+        //     public static Nested CreateInstance()
+        //     {
+        //         return new Nested();
+        //     }
+        // }
+        //
+        // public static Logger Inst => Nested.ClassInstance;
+
+        public static readonly ULogger UL = LogFactory.GetLogger();
+    }
+}
