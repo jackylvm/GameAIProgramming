52c9ea9e3ca25f6ec018680e1a94dfeeddb2b68c
jacky <jackylvm@foxmail.com>
2023-07-05 00:24:43 +0800

    MonoBehaviour绑定文件,程序执行入口

---
+++ Assets/Scripts/Chapter02/Runtime/Main/Chapter02_01.cs
@@ -0,0 +1,36 @@
+// *************************************************************************************
+// FileName: Chapter02_01.cs
+// Description:
+// 
+// Version: v1.0.0
+// Creator: Jacky(jackylvm@foxmail.com)
+// CreationTime: 2023-07-05 00:19:08
+// ==============================================================
+// History update record:
+// 
+// ==============================================================
+// *************************************************************************************
+
+using UnityEngine;
+
+namespace Chapter02.Runtime.Main
+{
+    public class Chapter02_01 : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+        private void FixedUpdate()
+        {
+        }
+    }
+}
