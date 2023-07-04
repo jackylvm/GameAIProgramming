b0f514158883cd6fbda66d414e94b16ac7789230
jacky <jackylvm@foxmail.com>
2023-07-05 00:22:50 +0800

    枚举值

---
+++ Assets/Scripts/Chapter02/Runtime/Enum/EmChapter02.cs
@@ -0,0 +1,29 @@
+﻿// *************************************************************************************
+// FileName: EmChapter02.cs
+// Description:
+// 
+// Version: v1.0.0
+// Creator: Jacky(jackylvm@foxmail.com)
+// CreationTime: 2023-07-04 23:45:33
+// ==============================================================
+// History update record:
+// 
+// ==============================================================
+// *************************************************************************************
+
+namespace Chapter02.Runtime.Enum
+{
+    public enum EmLocation
+    {
+        Shack,
+        Goldmine,
+        Bank,
+        Saloon
+    }
+
+    public enum EmEntity
+    {
+        EntMinerBob,
+        EntElsa
+    }
+}
