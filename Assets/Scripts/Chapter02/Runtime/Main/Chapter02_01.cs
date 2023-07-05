411a80a7143ddd84d10b9959676e58dc5722c06b
jacky <jackylvm@foxmail.com>
2023-07-05 08:17:47 +0800

    创建Miner并Update

--- Assets/Scripts/Chapter02/Runtime/Main/Chapter02_01.cs
+++ Assets/Scripts/Chapter02/Runtime/Main/Chapter02_01.cs
@@ -11,22 +11,29 @@
 // ==============================================================
 // *************************************************************************************
 
+using Chapter02.Runtime.Entity;
+using Chapter02.Runtime.Enum;
 using UnityEngine;
 
 namespace Chapter02.Runtime.Main
 {
     public class Chapter02_01 : MonoBehaviour
     {
+        [SerializeField] private Miner m_Miner;
+
         private void Awake()
         {
+            m_Miner = new Miner((int) EmEntity.EntMinerBob);
         }
 
         private void Start()
         {
+            m_Miner.DoStart();
         }
 
         private void Update()
         {
+            m_Miner.DoUpdate(Time.deltaTime);
         }
 
         private void FixedUpdate()
