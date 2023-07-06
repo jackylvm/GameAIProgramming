a9e6959bf3a1858d8063b4dbe0fbcc41df6fa256
jacky <jackylvm@foxmail.com>
2023-07-06 09:14:22 +0800

    增加一些注释

--- Assets/Scripts/Chapter02/Runtime/Entity/Miner/Miner.cs
+++ Assets/Scripts/Chapter02/Runtime/Entity/Miner/Miner.cs
@@ -21,11 +21,29 @@ namespace Chapter02.Runtime.Entity
     [Serializable]
     public partial class Miner : BaseGameEntity
     {
+        /// <summary>
+        /// 当前所在位置
+        /// </summary>
         public EmLocation Location { get; set; }
 
+        /// <summary>
+        /// 身上携带的黄金数量
+        /// </summary>
         private int m_GoldCarried;
+
+        /// <summary>
+        /// 疲劳值
+        /// </summary>
         private int m_Fatigue;
+
+        /// <summary>
+        /// 饥渴值
+        /// </summary>
         private int m_Thirst;
+
+        /// <summary>
+        /// 存在银行里面的黄金数量
+        /// </summary>
         private int m_MoneyInBank;
 
         public Miner(int id) : base(id)
